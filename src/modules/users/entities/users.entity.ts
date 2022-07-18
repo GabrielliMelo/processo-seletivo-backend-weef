2397c49c631b925e3e5d246b59a15cd0425540bc
GabrielliMelo <gabrielli.meloo16@gmail.com>
2022-07-18 13:22:03 -0300

    feat: create user entity

---
+++ src/modules/users/entities/users.entity.ts
@@ -0,0 +1,35 @@
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { hashSync } from 'bcrypt';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @BeforeInsert()
+  hashPassword() {
+    this.password = hashSync(this.password, 10);
+  }
+}
